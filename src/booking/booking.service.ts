22a41c93c2f52018b9ab070ff87ec73e1297dda2
hangju0610 <phj950610@naver.com>
2023-08-18 00:03:27 +0900

    주석 추가

--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -5,6 +5,8 @@ import { SetDto } from './dto/set.dto';
 
 @Injectable()
 export class BookingService {
+  // RedisModule을 사용하기 위해 Inject에 Token 투입
+  // 이를 통해 Redis를 연결하여 사용이 가능하다
   constructor(@Inject('REDIS_CLIENT') private redis: Redis) {}
 
   async get(key: string): Promise<string> {
@@ -17,8 +19,10 @@ export class BookingService {
     return { success: true };
   }
   async publish(data: BookingDto) {
+    // 채널 설정
     const channel = 'Ticket';
 
+    // publish로 채널에 메세지 전달
     await this.redis.publish(channel, JSON.stringify(data));
 
     return { success: true };
